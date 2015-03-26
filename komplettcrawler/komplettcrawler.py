59f5472a71ee2fbfc4b70db6d053899bf7dd7b0b
Idar Vassdal <tjoida13@nith.no>
2015-03-26 14:21:36 +0100

    fixed multithreading, cleaned up code

--- komplettcrawler/komplettcrawler.py
+++ komplettcrawler/komplettcrawler.py
@@ -2,18 +2,18 @@ __author__ = 'idar'
 import requests
 import threading
 from bs4 import BeautifulSoup
+#TODO: get price?
 
 lock = threading.Lock()
 lock_list = threading.Lock()
-items = set()
-page_nr = 10087
+items = set() #standard set
+page_nr = 10087 #starting page nr, i.e. #10087 is RAM-page
 update = 1
 
 
 def komplett_spider(max_pages, thread_name):
     print('thread ' + thread_name + 'starting ' + str(max_pages) + ' iterations')
     page = 1
-    item = 0
     while page <= max_pages:
         url = "https://www.komplett.no/k/kc.aspx?bn=" + str(get_page_nr())
         print(thread_name + 'Visiting ' + url)
@@ -31,6 +31,7 @@ def komplett_spider(max_pages, thread_name):
 def add_to_list(name):
     lock_list.acquire()
     print('Adding ' + name + ' to list')
+    global items
     items.add(name)
     lock_list.release()
 
@@ -40,7 +41,7 @@ def get_page_nr():
     global page_nr
     page_nr += 1
     a = page_nr
-    print(str(a))
+    print(str(a) + 'visited by' + threading.current_thread().name)
     lock.release()
     return a
 
@@ -59,25 +60,41 @@ def write_to_file(item_list):
 
 def start_threads():
     try:
-        t1 = threading.Thread(target=komplett_spider, args=(5, 't1'))
-        t2 = threading.Thread(target=komplett_spider, args=(3, 't2'))
+        # Set threads
+        t1 = threading.Thread(target=komplett_spider, args=(100, 't1'))
+        t2 = threading.Thread(target=komplett_spider, args=(100, 't2'))
+        t3 = threading.Thread(target=komplett_spider, args=(100, 't3'))
+        t4 = threading.Thread(target=komplett_spider, args=(100, 't4'))
+        t5 = threading.Thread(target=komplett_spider, args=(100, 't5'))
+        # Make threads daemon
         t1.daemon = False
         t2.daemon = False
+        t3.daemon = False
+        t4.daemon = False
+        t5.daemon = False
+        # Start threads
         t1.start()
         t2.start()
+        t3.start()
+        t4.start()
+        t5.start()
+        # Join threads to main thread
         t1.join()
         t2.join()
+        t3.join()
+        t4.join()
+        t5.join()
 
 
         print('Writing to file..')
         write_to_file(items)
-        #_thread.start_new_thread(komplett_spider, (5,w))
-        #_thread.start_new_thread(komplett_spider, (3,))
+        print("done")
     except:
         print('Error: unable to start thread')
 
 
+####################################
+########## Program start ###########
+####################################
 
 start_threads()
-#write_to_file(list(items).sort())
-#komplett_spider(1)
