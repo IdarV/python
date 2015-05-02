19ee3c230b47d185f3f827de4ba0688e8598abed
Idar Vassdal <tjoida13@nith.no>
2015-05-02 20:42:41 +0200

    renamed vgcrawler to vgnewsfetcher, commented code

---
+++ vgnewsfetcher/main.py
@@ -0,0 +1,28 @@
+__author__ = 'idar'
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = 'http://www.vg.no/'
+
+
+# Gets all news headers from ^url
+def vg_crawler():
+    data = requests.get(url)
+    page = data.text
+    soup = BeautifulSoup(page)
+    index = 0
+
+    for title in soup.findAll("div", {"class": "article-content"}):
+        # Strip name for newlines
+        name = str(title.get_text()).rstrip()
+        if re.search('[a-zA-Z]', name):
+            # Remove all non-letters and unnecessary whitespaces, print and increase index
+            name = " ".join(re.sub(r'[^\w]', ' ', name).split())
+            print(str(index) + ') ' + name + '\n')
+            index += 1
+
+
+
+# PROGRAM RUN
+vg_crawler()
