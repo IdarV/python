7196f00fb3b5bc9866073526c1b98c75115b8fe3
Idar Vassdal <tjoida13@nith.no>
2015-05-06 23:14:01 +0200

    poc: praw integration, but slow

--- redditnewestposts/redditnewestpost.py
+++ redditnewestposts/redditnewestpost.py
@@ -1,3 +1,10 @@
-print("Hello")
-print(" ")
-print("World")
+import praw
+import time
+
+
+r = praw.Reddit(user_agent='redditnewestpost')
+for y in range(1, 100):
+    time.sleep(1)
+    submissions = r.get_new(limit=5)
+    for x in submissions:
+        print(str(x))
