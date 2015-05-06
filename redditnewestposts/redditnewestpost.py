66c53e646045e7ca556f9790b585942fafd4bd63
Idar Vassdal <tjoida13@nith.no>
2015-05-06 23:50:22 +0200

    reset to prow

--- redditnewestposts/redditnewestpost.py
+++ redditnewestposts/redditnewestpost.py
@@ -1,9 +1,9 @@
 import praw
 import time
 
-
+#as reddit handles spam pretty well, this is the best option though its slow
 r = praw.Reddit(user_agent='redditnewestpost')
-for y in range(1, 100):
+for y in range(1, 2):
     time.sleep(1)
     submissions = r.get_new(limit=5)
     for x in submissions:
